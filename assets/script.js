0a145cc22d46fddb73da4fd935ee421e4103a1cd
Inez Escandon <iescan4@gmail.com>
2020-04-30 10:37:38 -0500

    added continue button

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,7 @@ var submitHighscoresPg = document.querySelector("#submit-highscores-page");
 var viewHighscoresPg = document.querySelector("#view-highscores-page");
 var questionEl = document.querySelector("#question");
 var choicesEl = document.querySelector("#choices");
+var continueBtn = document.querySelector("#continue");
 var finalscoreEl = document.querySelector("#finalscore");
 var submitBtn = document.querySelector("#submit");
 var goBackBtn = document.querySelector("#go-back");
@@ -22,9 +23,10 @@ function startTimer() {
     //add timer here
 }
 
-function reset () {
+function resetApp () {
     viewHighscoresPg.classList.add("hide");
     openingPg.classList.remove("hide");
+    feedbackEl.classList.add("hide");
     currentQuestionIndex = 0;
     score = 0;
     time = questions.length * 10;
@@ -35,7 +37,7 @@ function viewHighscores (event) {
     event.preventDefault();
     submitHighscoresPg.classList.add("hide");
     viewHighscoresPg.classList.remove("hide");
-    goBackBtn.addEventListener("click", reset)
+    goBackBtn.addEventListener("click", resetApp)
 
 }
 
@@ -44,26 +46,28 @@ function submitHighscores () {
     submitHighscoresPg.classList.remove("hide");
     finalscoreEl.textContent = (score * 10) + "%";
     submitBtn.addEventListener("click", viewHighscores);
-    feedbackEl.classList.add("hide");
 }
 
 function analyzeAnswer () {
     if (this.value === questions[currentQuestionIndex].answer) {
         score++;
         feedbackEl.classList.remove("hide");
+        continueBtn.classList.remove("hide");
         feedbackEl.textContent = "Correct";
     } else {
         time = time - 5;
         timerEl.textContent = "Time: " + time;
         feedbackEl.classList.remove("hide");
+        continueBtn.classList.remove("hide");
         feedbackEl.textContent = "Incorrect";
     }
     currentQuestionIndex++;
-    // feedbackEl.classList.add("hide");
-    getQuestions();
+    continueBtn.addEventListener("click", getQuestions);
 }
 
 function getQuestions () {
+    feedbackEl.classList.add("hide");
+    continueBtn.classList.add("hide");
     if (currentQuestionIndex === questions.length) {
         submitHighscores ();
     } else {
